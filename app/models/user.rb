f65cb585981cff9b85e0a71de60e13d55a628b50
Zak Davis <zakdavis@gmail.com>
2012-05-09 00:20:59 -0500

    Last commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,4 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-end
+endb
